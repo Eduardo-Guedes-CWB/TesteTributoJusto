810f461952f8a41cf3931c355dba3a88e70f45e7
User <User@DESKTOP-J9OG4FQ>
2020-06-16 17:35:34 -0300

    Commit inicial projeto

---
+++ EduardoGuedes/Models/PedidoModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EduardoGuedes.Models
+{
+    public class PedidoModel
+    {
+        public int IdPedido { get; set; }
+        public int IdCliente { get; set; }
+        public int IdProduto { get; set; }
+        public int QtdProduto { get; set; }
+        public decimal VlrUntProduto { get; set; }
+        public decimal VlrTtlProduto 
+        { 
+            get { return QtdProduto * VlrUntProduto; } 
+        }
+    }
+}
